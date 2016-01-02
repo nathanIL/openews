6a244daf292dbf947f3e751d0c05940d42e90802
Nathan Abu <nathanabu@gmail.com>
2016-01-02 20:27:33 +0200

    Fix failing build

--- tests/test_language_processors.py
+++ tests/test_language_processors.py
@@ -4,12 +4,15 @@ from nltk.corpus import stopwords
 import unittest
 import os
 
-
 @unittest.skipIf(os.environ.get('TRAVIS', None) is not None, "Skipping in Travis CI builds")
 class TestTransformer(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self._transformer = Transformer(mongo_connection_record, stopwords.words('english'))
+        self._stpwrds = stopwords
+        self._mcr = mongo_connection_record
+
+    def setUp(self):
+        self._transformer = Transformer(self._mcr, self._stpwrds.words('english'))
 
     def test_stopwords(self):
         self.assertIsInstance(self._transformer.stopwords, list, "Are stopwords returned as a list?")
